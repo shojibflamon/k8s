f173723ff58565031f2ec8a2a177b2c3fce237e7
Shojib Flamon <shojib.seu@gmail.com>
2025-04-09 12:06:36 +0600

    Setup Kind Cluster. Introduce Namespace & Pod

---
+++ kind-cluster/kubectl-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+chmod +x kubectl
+
+sudo mv kubectl /usr/local/bin/kubectl
+
+kubectl version --client
+
+echo "kubectl installed successfully"
